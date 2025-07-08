04eaa65adefa440d3872d69d071cf886ed136e6c
Elsheshtawey1 <mohamedelsheshtawey1@gmail.com>
2025-07-09 01:35:14 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/economy-api": {
+//         target: "https://alive-roby-karimhashim2002-3cfd1cd3.koyeb.app",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/economy-api": {
-        target: "https://alive-roby-karimhashim2002-3cfd1cd3.koyeb.app",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
